5bc62da5e5e8a7752655057015dcda0a5d81b378
Benjamin Taylor <benjamin.taylor1234@aggiemail.usu.edu>
2019-09-10 14:41:59 -0600

    Convert Composite key from numbers into a word

--- hw1/src/com/company/password/Main.java
+++ hw1/src/com/company/password/Main.java
@@ -1,8 +1,43 @@
 package com.company.password;
 
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) { // First argument should be the composite key
+        // write your code here
+        char[][] polybiusSquare = new char[6][6];
+        String polybius = "E2RFZMYH30B7OQANUKPXJ4VWD18GC69IS5TL";
+        char[] polybius2 = polybius.toCharArray();
+        System.out.println(35/6);
+        for (int i = 0; i < 36; i++) {
+            polybiusSquare[i/6][i%6] = polybius2[i];      }
+        for (int i = 0; i < 36; i++) {
+            if (i%6 == 0) System.out.println();
+            System.out.print(polybiusSquare[i/6][i%6]);
+        }
+
+        int oneTimePadKey = Integer.parseInt(args[0].substring(args[0].length()-2));
+        char[] columnarComposite = args[0].substring(0, args[0].length()-2).toCharArray();
+        System.out.println(columnarComposite);
+        System.out.println(oneTimePadKey);
+
+        String a = "a";
+        String b = "b";
+        String ab = a+b;
+        System.out.println(ab);
+
+        String compositeKey = getCompositeKey(columnarComposite, polybiusSquare);
+        System.out.println(compositeKey);
+    }
+
+    private static String getCompositeKey(char[] compositeNums, char[][] polybiusSquare) {
+        String compositeKey = "";
+        for (int i = 0; i < compositeNums.length; i+=2) {
+            int column = Character.getNumericValue(compositeNums[i]);
+            int row = Character.getNumericValue(compositeNums[i+1]);
+            System.out.println("Column " + column + " Row " + row);
+            compositeKey += polybiusSquare[column][row];
+        }
+        return compositeKey;
     }
 }
