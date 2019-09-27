1dd653a1ee70973a97cd8a6c9c2d48db14490e4f
Kaleb Lott <kelott5566@gmail.com>
2019-09-27 12:11:05 -0600

    Foolproofing the task 3 algorithm and get rid of some sysouts

--- hw2/src/BreakColumnar.java
+++ hw2/src/BreakColumnar.java
@@ -1,4 +1,7 @@
 public class BreakColumnar {
+
+    public static final int MAX_KEY_LENGTH = 20;
+
     public static int[] Cryptanalize(String p, String c) {
         int[] keys;
 
@@ -6,7 +9,6 @@ public class BreakColumnar {
         int i = 0;
         int column = 1;
 
-        System.out.println("I think the key width is " + Integer.toString(width));
         keys = new int[width];
         while(i < c.length()) {
             int pos = -1;
@@ -14,18 +16,15 @@ public class BreakColumnar {
                 pos++;
                 pos = getPositionWithinWidth(p, c.charAt(i), width, pos);
             } while (!checkWidth(p, c, i, pos, width));
-            System.out.println("It's at " + Integer.toString(pos) + ", " + Integer.toString(i));
             keys[pos] = column;
             column++;
 
-            System.out.println("Column Height: " + Integer.toString(getColumnHeight(p, width, pos)));
             i += getColumnHeight(p, width, pos);
         }
         return keys;
     }
 
     private static int getPositionWithinWidth(String a, char b, int width, int start) {
-        System.out.println("Looking for " + b + " within " + a.substring(0, width));
         for(int i = start; i <= width; i++) {
             char test = a.charAt(i);
             if(test == b) {
@@ -40,7 +39,7 @@ public class BreakColumnar {
     }
 
     private static boolean checkWidth(String p, String c, int cipherStart, int plainStart, int width) {
-        for(int i = 0; i <= width; i++) {
+        for(int i = 0; i <= p.length(); i++) {
             if(cipherStart + i >= c.length()) return true;
             char cipherChar = c.charAt(cipherStart + i);
             if(plainStart + (i * width) >= p.length()) return true;
@@ -52,10 +51,10 @@ public class BreakColumnar {
     private static int getKeyWidth(String p, String c) {
         char first = c.charAt(0);
         int width; 
-        for(int i = 1; i <= 7; i++) {
+        for(int i = 1; i <= MAX_KEY_LENGTH; i++) {
             char test = p.charAt(i);
             if(test == first) {
-                for(width = 1; width <= 7; width++) {
+                for(width = 1; width <= MAX_KEY_LENGTH; width++) {
                     if(checkWidth(p, c, 0, i, width)) {
                         return width;
                     }
