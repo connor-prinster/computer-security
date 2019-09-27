02dc1dc6aac5bf8c2f0eaea6544777ca19e8c4bd
jazlyn-maxwell <jazlyn.maxwell@gmail.com>
2019-09-26 18:53:16 -0600

    'made initial classes for encryption and decryption'

---
+++ hw2/src/functions/Decryption.java
@@ -0,0 +1,17 @@
+/**
+ * Decryption contains the private data members private_key and ciphertext. The method decrypt()
+ * uses private_key, a private rsa key, to decrypt the encoded ciphertext into a plaintext message.
+ */
+public class Decryption {
+    private String private_key;
+    private String ciphertext;
+
+    public Decryption(String private_key, String ciphertext) {
+        this.private_key = private_key;
+        this.ciphertext = ciphertext;
+    }
+    String decrypt(String private_key, String ciphertext) {
+        String plaintext = ciphertext;
+        return plaintext;
+    }
+}
