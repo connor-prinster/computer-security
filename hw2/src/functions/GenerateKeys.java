3e7ed8010a64bad7dea71c0aae687f13deababb3
Connor Prinster <connor.prinster@gmail.com>
2019-10-02 13:50:56 -0600

    the primes will be different now

--- hw2/src/functions/GenerateKeys.java
+++ hw2/src/functions/GenerateKeys.java
@@ -3,10 +3,10 @@ package functions;
 import java.util.*;
 
 public class GenerateKeys {
-//    public static void main(String[] args) {
-//        GenerateKeys gk = new GenerateKeys();
-//        System.out.println(gk.getKeys());
-//    }
+    public static void main(String[] args) {
+        GenerateKeys gk = new GenerateKeys();
+        System.out.println(gk.getKeys());
+    }
 
     /**
      * generates map containing public and private keys
@@ -15,10 +15,8 @@ public class GenerateKeys {
     public static Map<String, Map<String, Integer>> getKeys() {
 
         // generate the private key
-        KeyTuple publicKey = generatePublic(
-                generatePrimes(Math.random()),
-                generatePrimes((Math.random()))
-        );
+        ArrayList<Integer> rands = generatePrimes(Math.random(), Math.random());
+        KeyTuple publicKey = generatePublic(rands.get(0), rands.get(1));
 
         // using the public key, generate the private one
         KeyTuple privateKey = generatePrivate(publicKey);
@@ -48,7 +46,8 @@ public class GenerateKeys {
      * @return returns a KeyTuple holding the e-value and the n-value for public keys
      */
     private static KeyTuple generatePublic(int p, int q) {
-        // public key is very easily generated using the algorithms shown below
+
+        System.out.println("p: " + p + " q: " + q);
         int m = p * q;
         int fn = (p - 1) * (q - 1);
         int rpn = genRpn(fn);
@@ -113,16 +112,28 @@ public class GenerateKeys {
 
     /**
      * will generate an integer prime value given a double value from Math.random()
-     * @param val the randomly generated value (will be less than 1)
-     * @return a randomly selected prime value
+     * @param val1 the randomly generated value (will be less than 1)
+     * @param val2 the randomly generated value (will be less than 1)
+     * @return an ArrayList of two randomly chosen prime numbers
      */
-    private static int generatePrimes(double val) {
+    private static ArrayList<Integer> generatePrimes(double val1, double val2) {
         ArrayList<Integer> arr = new ArrayList<>();
         // list of prime values. This is cut down to avoid too large of an 'm' value
         Collections.addAll(arr, 2,3,5,7,11,13,17,19,23,29,31,37);//,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997,1009,1013,1019,1021,1031,1033,1039,1049,1051,1061,1063,1069,1087,1091,1093,1097,1103,1109,1117,1123,1129,1151,153,1163,1171,1181,1187,1193,1201,1213,1217,1223,1229,1231,1237,1249,1259,1277,1279,1283,1289,1291,1297,1301,1303,1307,1319,1321,1327,1361,1367,1373,1381,1399,1409,1423,1427,1429,1433,1439,1447,1451);
+
         // as val will be less than 0 due to Math.rand(), multiply it by 1000 and cast to an int
-        int randIdx = (int)(val * 1000) % arr.size();
-        // get the randomly chosen index
-        return arr.get(randIdx);
+        int idx1 = (int)(val1 * 1000) % arr.size();
+        int idx2 = (int)(val2 * 1000) % arr.size();
+
+        // if the indexes are the same, idx2 must be changed
+        while(idx1 == idx2) {
+            idx2 = (int)(val2 * 1000) % arr.size();
+        }
+
+        // a pair of randomly chosen primes
+        ArrayList<Integer> pair = new ArrayList<>();
+        pair.add(arr.get(idx1));
+        pair.add(arr.get(idx2));
+        return pair;
     }
 }
