6ce436e6ecd178bdeed0b62044d49843343b260c
Magicpig55 <kelott5566@gmail.com>
2019-10-01 20:43:00 -0600

    Fixed encryption and decryption

--- hw2/src/functions/Decryption.java
+++ hw2/src/functions/Decryption.java
@@ -1,18 +1,29 @@
 package functions;
 
+import java.math.BigInteger;
+import java.nio.charset.Charset;
+import java.util.Base64;
+
 /**
  * Decryption contains the private data members private_key and ciphertext. The method decrypt()
  * uses private_key, a private rsa key, to decrypt the encoded ciphertext into a plaintext message.
  */
 public class Decryption {
-    static String decrypt(double pd, double m, String ciphertext) {
-        StringBuilder plaintext = new StringBuilder();
-        char[] plainCharacters = ciphertext.toCharArray();
-        for (char c : plainCharacters) {
-            char encode = (char) (Math.pow(c, pd) % m);
-            System.out.println(String.format("%d decrypts to %d", (int)c, (int)encode));
-            plaintext.append(encode);
+    static String decrypt(int pd, int m, String ciphertext) {
+        BigInteger exponent = new BigInteger(Integer.toString(pd));
+        BigInteger modulus = new BigInteger(Integer.toString(m));
+
+        char[] cipherCharacters = new String(Base64.getDecoder().decode(ciphertext), Charset.defaultCharset()).toCharArray();
+
+        StringBuilder plainText = new StringBuilder();
+
+        for (char c : cipherCharacters) {
+            BigInteger encrypted = new BigInteger((Integer.toString((int)c)));
+            BigInteger decrypted = encrypted.modPow(exponent, modulus);
+            plainText.append((char)decrypted.intValue());
         }
-        return plaintext.toString();
+
+
+        return plainText.toString();
     }
 }
--- hw2/src/functions/Encryption.java
+++ hw2/src/functions/Encryption.java
@@ -1,26 +1,41 @@
 package functions;
 
+import java.math.BigInteger;
+import java.util.Base64;
+
 /**
  * Encryption contains the private data members public_key and plaintext. The method encrypt()
  * uses public_key, a public rsa key, to encrypt the plaintext message into an encoded ciphertext.
  */
 public class Encryption {
-    static String encrypt(double rpn, double m, String plaintext) {
-        StringBuilder ciphertext = new StringBuilder();
+    static String encrypt(int rpn, int m, String plaintext) {
         char[] plainCharacters = plaintext.toCharArray();
+        StringBuilder numberRepresentation = new StringBuilder();
+
+        BigInteger exponent = new BigInteger(Integer.toString(rpn));
+        BigInteger modulus = new BigInteger(Integer.toString(m));
+
         for (char c : plainCharacters) {
-            char encode = (char)(Math.pow(c, rpn) % m);
-            System.out.println(String.format("%d decrypts to %d", (int)c, (int)encode));
-            ciphertext.append(encode);
+            BigInteger message = new BigInteger(Integer.toString(c));
+
+            BigInteger encrypted = message.modPow(exponent, modulus);
+
+            numberRepresentation.append((char)encrypted.intValue());
+            
+            // char encode = (char)(Math.pow(c, rpn) % m);
+            // System.out.println(String.format("%d decrypts to %d", (int)c, (int)encode));
+            // ciphertext.append(encode);
+
         }
-        return ciphertext.toString();
+
+        return Base64.getEncoder().encodeToString(numberRepresentation.toString().getBytes());
     }
 
     public static void main(String[] args) {
-        String cipherText = encrypt(7, 55, "Hello world");
+        String cipherText = encrypt(17, 3233, "Hello world! I can't believe this works!!");
         System.out.println(cipherText);
-        System.out.println(Decryption.decrypt(23, 55, cipherText));
-        // public rpn 9 m 319
+        System.out.println(Decryption.decrypt(413, 3233, cipherText));
+        // public rpn 17 m 3233
         // private 249 319
     }
 }
