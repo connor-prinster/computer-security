1b6c5de6f4e6f699e630270f0084a8320655d5fc
jazlyn-maxwell <jazlyn.maxwell@gmail.com>
2019-10-01 18:09:25 -0600

    made method decrypt in functions/Decryption.

--- hw2/src/functions/Decryption.java
+++ hw2/src/functions/Decryption.java
@@ -5,15 +5,13 @@ package functions;
  * uses private_key, a private rsa key, to decrypt the encoded ciphertext into a plaintext message.
  */
 public class Decryption {
-    private String private_key;
-    private String ciphertext;
-
-    public Decryption(String private_key, String ciphertext) {
-        this.private_key = private_key;
-        this.ciphertext = ciphertext;
-    }
-    String decrypt(String private_key, String ciphertext) {
-        String plaintext = ciphertext;
-        return plaintext;
+    String decrypt(double pd, double m, String ciphertext) {
+        StringBuilder plaintext = new StringBuilder();
+        char[] plainCharacters = ciphertext.toCharArray();
+        for (char c : plainCharacters) {
+            char encode = (char)(Math.pow(c, pd) % m);
+            plaintext.append(encode);
+        }
+        return plaintext.toString();
     }
 }
