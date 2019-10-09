d04fd4ff6cd2a87cbbb8036b7d4cd80b83e7a790
Kaleb Lott <kelott5566@gmail.com>
2019-10-09 12:41:18 -0600

    start on bitshift logic

--- hw3/src/Main.java
+++ hw3/src/Main.java
@@ -2,10 +2,16 @@ package PACKAGE_NAME;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        
     }
 
     void hash() {
 
+    }
+
+    static int RotateLeftTwice(int a) {
+        int b = a << 2;
+        int c =  (a & 0xC0000000) / (0xC0000000);
+        return b + c;
     }
 }
