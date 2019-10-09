b7fa77b4e103766dce7406e89683603bea68b250
Benjamin Taylor <benjamin.taylor1234@aggiemail.usu.edu>
2019-10-08 18:01:05 -0600

    Split into 224 bit chunks

--- hw3/src/Main.java
+++ hw3/src/Main.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[]args){
+        String message = args[0];
+        ArrayList<String> chunks = divideInto224BitChuncks(message);
+    }
+
+    private static ArrayList<String> divideInto224BitChuncks(String message) {
+        ArrayList<String> chunks = new ArrayList<>();
+        while (message.length() / 28 != 0) {
+            chunks.add(message.substring(0,28));
+            message = message.substring(28);
+        }
+        if (message.length() % 28 == 0) {
+            return chunks;
+        }
+        else {
+            while (message.length() < 28) {
+                message += "0";
+            }
+            chunks.add(message);
+            return chunks;
+        }
+    }
 }
