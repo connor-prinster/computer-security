892323184656db47ea8f99844c5390d1471f1654
Magicpig55 <kelott5566@gmail.com>
2019-11-19 17:45:18 -0700

    union checking with sqli

--- hw5/src/SQLI.java
+++ hw5/src/SQLI.java
@@ -1,3 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SQLI {
-    
+    private String statement;
+
+    public SQLI(String statement) {
+        this.statement = statement;
+    }
+
+    private int checkUnion(String str) {
+        String union = "union";
+        return matchCount(union, str);
+    }
+
+    private int matchCount(String regex, String test) {
+        Pattern r = Pattern.compile(regex);
+        Matcher m = r.matcher(test);
+        int count = 0;
+        while(m.find()) count++;
+        return count;
+    }
 }
